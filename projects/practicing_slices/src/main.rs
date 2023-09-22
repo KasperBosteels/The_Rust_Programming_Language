81bbef4e081a0a1d238564d45b68e61089075446
Kasper B. <kasperbosteels@hotmail.com>
2023-09-22 17:12:26 +0200

    slices finished

--- projects/practicing_slices/src/main.rs
+++ projects/practicing_slices/src/main.rs
@@ -2,14 +2,34 @@ fn main() {
     let s = String::from("hello world hello1");
     let word = first_word(&s);
     println!("The first word is: {}", word);
+    let second_word = second_word(&s);
+    println!("The second word is: {}", second_word);
+    slicing();
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes(); // convert string to array of bytes
     for (i, &item) in bytes.iter().enumerate() { // iter returns each element in a collection and enumerate wraps the result of iter and returns each element as part of a tuple instead
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
+}
+
+fn second_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[i..];
+        }
+    }
+    &s[..]
+}
+fn slicing () {
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("  {} {}", hello, world);
 }
