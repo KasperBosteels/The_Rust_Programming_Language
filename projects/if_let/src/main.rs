7fff598da533402ed6bca81de7459b01bae774ca
Kasper B <kasperbosteels@hotmail.com>
2023-12-16 15:31:55 +0100

    if let with enums

--- projects/if_let/src/main.rs
+++ projects/if_let/src/main.rs
@@ -1,4 +1,14 @@
+enum State{
+    Antwerp,
+    NewYork,
+    Boedapesht
+}
 
+enum Coin{
+    Cent(i16),
+    Quarter(State),
+    Euro(u8)
+}
 fn main() {
     let config_max = Some(3u8);
 
@@ -9,5 +19,16 @@ fn main() {
 
     if let Some(max) = config_max {
         println!("The maximum from if let is to be {max}")
+    }
+    let coin = Coin::Quarter(State::NewYork);
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state)=> println!("State quarter from {state}"),
+        _ => count +=1,
+    }
+    if let Coin::Quarter(state) = coin {
+        println!("This quarter is from the state {state}");
+    }else{
+        count+=1;
     }
 }
