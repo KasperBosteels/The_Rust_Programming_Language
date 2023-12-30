f959c5c273e3afe6e8f957144fa1a2ad04a44427
Kasper B <kasperbosteels@hotmail.com>
2023-12-30 15:44:19 +0100

    usage of public enum from module

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -11,20 +11,16 @@ mod front_of_house {
     }
 }
 pub fn eat_at_restaurant(){
-    // order a breakfast in the summer with rye toast.
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // Change our mind about what bread we'd like.
-    meal.toast = String::from("Wheat");
-    println("I'd like a {} toast please.", meal.toast);
-
-    // The next line will not compile if we uncomment it; we're not
-    // allowed to see or modify the seasonal fruit that comes with the meal.
-    // meal.seasonal_fruit = String::from("blueberries");
+    let order1 = back_of_house::Appetizer::Salad;
+    let order2 = back_of_house::Appetizer::Soup;
 }
 
 fn deliver_order() {}
 mod back_of_house {
-
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
     pub struct Breakfast {
         pub toast:String,
         seasonal_fruit : String,
