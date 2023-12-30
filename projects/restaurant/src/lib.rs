c5766217ffd86317aebff001c40e012028602d92
Kasper B <kasperbosteels@hotmail.com>
2023-12-30 16:09:00 +0100

    idem

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -13,7 +13,7 @@ mod front_of_house {
 pub use crate::front_of_house::hosting;
 
 mod customer {
-    use create::front_of_house::hosting;
+    pub use create::front_of_house::hosting;
     pub fn eat_at_restaurant(){
         hosting::add_to_waitlist();
     }
