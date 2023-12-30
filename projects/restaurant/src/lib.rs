61f6209838d1ec29c97dc8657b1e6f91407743d3
Kasper B <kasperbosteels@hotmail.com>
2023-12-30 15:35:57 +0100

    modules paths and publics

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -9,4 +9,33 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payments() {}
     }
+}
+pub fn eat_at_restaurant(){
+    //absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    //relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+mod back_of_house{
+    pub struct Breakfast {
+        pub toast:String,
+        seasonal_fruit : String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+           Breakfast {
+               toast: String::from(toast),
+               seasonal_fruit: String::from("peaches"),
+           }
+        }
+    }
+
+        fn fix_incorrect_order() {
+            cook_order();
+            super::deliver_order();
+        }
+    fn cook_order() {}
 }
