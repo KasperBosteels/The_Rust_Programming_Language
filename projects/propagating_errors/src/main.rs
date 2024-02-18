c2c8330d8b952f7535e8f731082b9289c3ba44bf
Kasper Bosteels <kasperbosteels@hotmail.com>
2024-02-18 15:39:18 +0100

    returning the exceptions

--- projects/propagating_errors/src/main.rs
+++ projects/propagating_errors/src/main.rs
@@ -1,18 +1,17 @@
 use std::fs::File;
 use std::io::{self, Read};
 
-
-
-
-fn main() {
-
-
+fn main() -> Result<(), Box <dyn Error>> {
+    let greeting_file =File::open("hello.txt")?;
+    Ok(())
 }
 
-
 fn read_username_from_file()  ->  Result<String, io::Error> {
-    let mut username_file = File::open("hello.txt")?;
     let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    Ok(username);
+    let mut username_file = File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn last_char_of_first_line(text:&str) ->  Option<char> {
+    text.lines().next()?.chars().last()
 }
