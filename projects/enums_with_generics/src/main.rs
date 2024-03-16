4ade1ee90ea9f9a2b9a89aa5ac2480ed86131ddb
Kasper Bosteels <kasperbosteels@hotmail.com>
2024-03-16 13:04:58 +0100

    meh

--- projects/enums_with_generics/src/main.rs
+++ projects/enums_with_generics/src/main.rs
@@ -9,6 +9,24 @@ enum Result<T,E>{
     Err(E),
 }
 
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
-    
+    let p = Point {x:5, y:10};
+    println!("p.x = {}", p.x());
 }
