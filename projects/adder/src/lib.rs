190248d134fc67f34dc9326b1588ad08f868bab1
Kasper B <kasperbosteels@hotmail.com>
2024-05-11 12:23:36 +0200

    this too

--- projects/adder/src/lib.rs
+++ projects/adder/src/lib.rs
@@ -2,6 +2,18 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width:u32,
+    height:u32,
+}
+
+impl Rectangle {
+    fn Can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,7 +25,38 @@ mod tests {
     }
 
     #[test]
+    #[should_panic]
     fn another(){
         panic!("This test should fail");
+    }
+
+    #[test]
+    fn larger_Can_hold_smaller(){
+        let larger = Rectangle{
+            height: 8,
+            width: 8,
+        };
+
+        let smaller = Rectangle{
+            width: 3,
+            height: 7
+        };
+        let value = larger.Can_hold(&smaller);
+        assert_eq!(true,value);
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger(){
+        let larger = Rectangle{
+            height: 8,
+            width: 8,
+        };
+
+        let smaller = Rectangle{
+            width: 3,
+            height: 7
+        };
+
+        assert!(!smaller.Can_hold(&larger));
     }
 }
