3079ed7235eb5cbe6fb53529ce077e37ae23d85c
Kasper B. <kasperbosteels@hotmail.com>
2024-08-26 23:19:07 +0200

    stuff

--- projects/smart_pointers/src/main.rs
+++ projects/smart_pointers/src/main.rs
@@ -25,6 +25,45 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T> where T: Messenger {
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+}
+
+pub fn set_value(&mut self, value:usize) {
+    self.value = value;
+
+    let percentage_of_max = self.value as f64 / self.max as f64;
+
+    if percentage_of_max >= 1.0 {
+        self.messenger.send("you are over quota");
+    } else if percentage_of_max >= 0.9 {
+        self.messenger.send("you are on quota");
+    }else if percentage_of_max >= 0.75 {
+        self.messenger.send("you are 3/4 on you quota");
+    }
+}
+
+
+
+
+
+
 fn main() {
     /*
     // stores a value in the heap with a pointer to it in the stack
