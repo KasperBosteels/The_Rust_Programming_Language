95e80aed952d3f45758eca5ca64e25f2262e7cc6
Kasper Bosteels <kasperbosteels@hotmail.com>
2024-12-22 20:20:56 +0100

    ownership

--- zenva/ownership_zenva/src/main.rs
+++ zenva/ownership_zenva/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
+    let s1 = String::from("hello");
 
-let s1 = String::from("rob");
+    let s2 = s1.clone();
 
+    println!("{s2} world");
+    println!("{s1} world");
 
+    let name = String::from("rob");
+    let name = print_name(name);
+    println!("{name}");
+}
+
+fn print_name(name: String) -> String {
+    println!("welcome {}", name);
+    name
 }
