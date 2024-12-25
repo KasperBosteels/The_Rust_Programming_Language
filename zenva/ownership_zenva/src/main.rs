4b5904c5db116c620fcabbb483bad437d9a60ca7
Kasper Bosteels <kasperbosteels@hotmail.com>
2024-12-25 15:51:27 +0100

    ownership

--- zenva/ownership_zenva/src/main.rs
+++ zenva/ownership_zenva/src/main.rs
@@ -9,9 +9,35 @@ fn main() {
     let name = String::from("rob");
     let name = print_name(name);
     println!("{name}");
+
+//reference
+
+let name = String::from("rob");
+print_greeting(&name);
+println!("{name}");
+    //part 2
+
+    //slice
+let message = String::from("Hello world");
+
+let hello = &message[0..5];
+println!("{hello}");
+
+
+
+let array = [1,2,3,4,5];
+
+let slice = &array[1..4];
+for num in slice {
+    println!("{num}");
+}
 }
 
 fn print_name(name: String) -> String {
     println!("welcome {}", name);
     name
+}
+
+fn print_greeting (name : &String) {
+    println!("Welcome {}", name);
 }
